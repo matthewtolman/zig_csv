fb3516b7570ca9607502d64b5f07cf4d5d7d00b2
mtolman <mtolman@matthewtolman.dev>
2025-05-28 23:07:21 -0600

    bug fix, updated build

--- src/common.zig
+++ src/common.zig
@@ -76,7 +76,7 @@ const std = @import("std");
 /// Assumes the field is a valid CSV field
 pub fn unquoteQuoted(data: []const u8, opts: CsvOpts) []const u8 {
     std.debug.assert(opts.valid());
-    if (data.len > 2 and data[0] == opts.column_quote) {
+    if (data.len >= 2 and data[0] == opts.column_quote) {
         // If we get here, we should be working on quoted CSV data
         std.debug.assert(data[data.len - 1] == opts.column_quote);
         return data[1..(data.len - 1)];
